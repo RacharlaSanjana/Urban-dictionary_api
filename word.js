a7e4d30fb5bd0d5c3b5f491ea62c13ca01371d4a
RacharlaSanjana <114012057+RacharlaSanjana@users.noreply.github.com>
2023-03-05 15:37:44 +0530

    Add files via upload

---
+++ word.js
@@ -0,0 +1,38 @@
+const Var=document.getElementById("#input")
+const kill = document.querySelector("#buttonss");
+const mean = document.querySelector("#meaning");
+const Synonyms = document.querySelector("#synonym");
+kill.addEventListener("submit",function(e)
+{
+
+    e.preventDefault();
+    console.log(input.value);
+
+const apiKey = "1f67d1f337mshc2a539b03ac37aep1ced8djsn5a177350d8e0";
+const apiUrl = "https://mashape-community-urban-dictionary.p.rapidapi.com/define";
+
+// Define a function to retrieve the data
+async function getDictionaryData(word) {
+  const url = `${apiUrl}?term=${word}`;
+  const response = await fetch(url, {
+    headers: {
+      "x-rapidapi-key": apiKey,
+      "x-rapidapi-host": "mashape-community-urban-dictionary.p.rapidapi.com"
+    }
+  });
+  const data = await response.json();
+  return data;
+}
+
+const word=input.value;
+// Example usage
+getDictionaryData(word).then(data => {
+  meaning=(`Meaning of ${word}: ${data.list[0].definition}`);
+  mean.innerHTML=meaning;
+  synn=(`Synonyms of ${word}: ${data.list[0].example}`);
+  Synonyms.innerHTML=synn;
+
+}).catch(error => {
+  console.error(`Error retrieving data for ${word}: ${error}`);
+});
+})
